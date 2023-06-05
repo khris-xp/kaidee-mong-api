9c69ccc239fbbae61dbe353f61c78689acab8939
khris-xp <khrisbharmmano@gmail.com>
2023-06-05 15:11:35 +0700

    feat: connect to mongodb

--- server.ts
+++ server.ts
@@ -1,9 +1,19 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
+import fileUpload from 'express-fileupload';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({
+    useTempFiles: true
+}));
 const port = 8000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -13,3 +23,7 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`⚡️ Server is running at http://localhost:${port}`);
 });
+
+const MONGODB_URL: string | undefined = process.env.MONGO_URL;
+
+mongoose.connect(MONGODB_URL as string).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
