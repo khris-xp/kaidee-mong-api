339cf456c77fb8480160443f734afa8e854b4cf4
khris-xp <khrisbharmmano@gmail.com>
2023-06-08 20:09:56 +0700

    feat: upload video routes

--- routes/upload.routes.ts
+++ routes/upload.routes.ts
@@ -5,12 +5,16 @@ import { Request, Response } from 'express';
 import fs from 'fs';
 import authUser from '../middleware/auth';
 import authAdmin from '../middleware/authAdmin';
+const multer = require('multer');
+const streamifier = require('streamifier');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET,
 });
+
+const upload = multer({ dest: 'temp/' });
 
 uploadRouter.post('/upload', authUser, authAdmin, (req: Request, res: Response) => {
     try {
@@ -19,19 +23,30 @@ uploadRouter.post('/upload', authUser, authAdmin, (req: Request, res: Response) 
             return res.status(400).json({ msg: "No files were uploaded." });
         }
 
+        const fileStream = streamifier.createReadStream(req.files.buffer);
+
         const file = Array.isArray(req.files.file) ? req.files.file[0] : req.files.file;
 
-        if (file.size > 1024 * 1024) {
-            removeTmp(file.tempFilePath);
-            return res.status(400).json({ msg: "Size too large." });
-        }
-
-        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
-            removeTmp(file.tempFilePath);
-            return res.status(400).json({ msg: "File format is incorrect." });
-        }
-
         cloudinary.uploader.upload(file.tempFilePath, { folder: "KAIDEE MONG" }, async (err: cloudinaryResult.UploadApiErrorResponse | null, result: cloudinaryResult.UploadApiResponse | undefined) => {
+            if (err) throw err;
+            removeTmp(file.tempFilePath);
+            res.json({ public_id: result?.public_id, url: result?.secure_url });
+        })
+    } catch (err: any) {
+        return res.status(500).json({ msg: err.message });
+    }
+})
+
+uploadRouter.post('/upload/video', authUser, authAdmin, (req: Request, res: Response) => {
+    try {
+
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).json({ msg: "No files were uploaded." });
+        }
+
+        const file = Array.isArray(req.files.file) ? req.files.file[0] : req.files.file;
+
+        cloudinary.uploader.upload(file.tempFilePath, { resource_type: "video", folder: "KAIDEE MONG" }, async (err: cloudinaryResult.UploadApiErrorResponse | null, result: cloudinaryResult.UploadApiResponse | undefined) => {
             if (err) throw err;
             removeTmp(file.tempFilePath);
             res.json({ public_id: result?.public_id, url: result?.secure_url });
